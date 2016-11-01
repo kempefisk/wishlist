b34b951fdde0007ab60c475778ea731398d1742f
kempefisk <kempefisk@users.noreply.github.com>
2016-11-01 13:53:48 +0100

    Add files via upload

--- js/firebase.js
+++ js/firebase.js
@@ -1,12 +1,15 @@
 var provider = new firebase.auth.GoogleAuthProvider();
+var user
 
 function signIn() {
 	firebase.auth().signInWithPopup(provider).then(function(result) {
 	  // This gives you a Google Access Token. You can use it to access the Google API.
 	  var token = result.credential.accessToken;
 	  // The signed-in user info.
-	  var user = result.user;
-	  // ...
+	  user = result.user;
+	  // Input Welcome text
+	  $("#welcomeText").html("Hej " + user.DisplayName);
+	  
 	}).catch(function(error) {
 	  // Handle Errors here.
 	  var errorCode = error.code;
